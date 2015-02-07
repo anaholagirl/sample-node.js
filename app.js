be89a203a9c331022cca40dd83b6c6c68a139de0
Linda Gibson <dakidz@live.com>
2015-02-06 21:12:11 -0800

    can use argv property on process object to access command line arguments

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var profile = require("./profile.js");
-//global object for node.js
-console.dir(process);
+//global object for node.js, can access current version of node and arguments passed in as a command line
+var users = process.argv.slice(2);
+//command line arguments can be accessed through argv property on the process object
+//console.dir(process.argv);
 //var users = ["lindagibson2", "chalkers", "davemcfarland"];
 //
 //users.forEach(profile.get);
