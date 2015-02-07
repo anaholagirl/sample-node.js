63e895c09b012f4f73d583b81c9740675fd2f2e0
Linda Gibson <dakidz@live.com>
2015-02-06 18:17:00 -0800

    now is an object and can print out badges and points using printMessage

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var request = http.get("http://teamtreehouse.com/" + username + ".json", functio
   });
   response.on('end', function(){
     var profile = JSON.parse(body);
-    console.dir(profile);
+    printMessage(username, profile.badges.length, profile.points.JavaScript);
+
   });
   //Parse the data
   //Print the data
