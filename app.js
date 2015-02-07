aa1a7320001298f231ebc49c522eb7a1865a4741
Linda Gibson <dakidz@live.com>
2015-02-06 20:50:51 -0800

    can create an array of users and do a loop to iterate and retreive each user's profile

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var profile = require("./profile.js");
+var users = ["chalkers", "lindagibson2"];
 
-profile.get("chalkers");
-profile.get("lindagibson2");
+users.forEach(function(username){
+  profile.get(username);
+});
+
+
 //normally data would be retrieved in order requested, but because node.js retrieves information as they complete the smaller file will show up first
