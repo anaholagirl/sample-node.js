1febaf417ce7ae07e7fd130146877e82d94fc240
Linda Gibson <dakidz@live.com>
2015-02-06 20:53:14 -0800

    refactor for cleaner code

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var profile = require("./profile.js");
 var users = ["chalkers", "lindagibson2"];
 
-users.forEach(function(username){
-  profile.get(username);
-});
+users.forEach(profile.get);
 
 
 //normally data would be retrieved in order requested, but because node.js retrieves information as they complete the smaller file will show up first
