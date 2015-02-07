7a7c169a823be7759859b73a235ef8debc09c9b2
Linda Gibson <dakidz@live.com>
2015-02-06 20:46:16 -0800

    can get more than one profile

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 var profile = require("./profile.js");
 
+profile.get("chalkers");
 profile.get("lindagibson2");
+//normally data would be retrieved in order requested, but because node.js retrieves information as they complete the smaller file will show up first
