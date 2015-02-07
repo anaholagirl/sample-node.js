80035690559e16621a2274cf378e3c96547dbd42
Linda Gibson <dakidz@live.com>
2015-02-06 20:59:58 -0800

    call up process which is a global object in node.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var profile = require("./profile.js");
-var users = ["chalkers", "lindagibson2"];
-
-users.forEach(profile.get);
+//global object for node.js
+console.dir(process);
+//var users = ["lindagibson2", "chalkers", "davemcfarland"];
+//
+//users.forEach(profile.get);
 
 
 //normally data would be retrieved in order requested, but because node.js retrieves information as they complete the smaller file will show up first
